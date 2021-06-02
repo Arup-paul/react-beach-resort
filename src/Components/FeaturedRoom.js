52f1c501e0a1290672f48b465c48365650a69f99
Arup paul <aruppaulctg@gmail.com>
2021-06-02 17:35:48 +0600

    feature room  showing

--- src/Components/FeaturedRoom.js
+++ src/Components/FeaturedRoom.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
 import {RoomContext} from "../Context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 class FeaturedRoom extends Component {
     static contextType = RoomContext;
 
     render() {
+       let {loading,featuredRooms : rooms} = this.context;
+
+         rooms = rooms.map(room => {
+           return (
+                    <Room key={room.id} room={room} />
+                )  });
 
         return (
-            <div>
-            welcome
-            </div>
+            <section className="featured-rooms">
+                <Title title="Featured Rooms"/>
+                   <div className="featured-rooms-center">
+                       {loading ? <Loading /> : rooms}
+                   </div>
+            </section>
         );
     }
 }
