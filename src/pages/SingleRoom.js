cf4076ad4bdfe9100158585771417438d6d2f8f9
Arup paul <aruppaulctg@gmail.com>
2021-06-02 12:30:03 +0600

    Routing setup

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                Hello single Room
+            </div>
+        );
+    }
+}
+
+export default SingleRoom;
