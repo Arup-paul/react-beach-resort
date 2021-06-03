751370d297ecf765d8b859807ea7a9e57e701440
Arup paul <aruppaulctg@gmail.com>
2021-06-03 14:15:53 +0600

    room list show

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Hero from "../Components/Hero";
 import Banner from "../Components/Banner";
 import {Link} from "react-router-dom";
-
+import RoomContainer from '../Components/RoomContainer'
 const Rooms = () => {
     return (
+        <>
         <Hero hero="roomsHero">
             <Banner
                 title="Our Rooms"
@@ -14,6 +15,8 @@ const Rooms = () => {
                 </Link>
             </Banner>
         </Hero>
+       <RoomContainer />
+        </>
     );
 }
 
